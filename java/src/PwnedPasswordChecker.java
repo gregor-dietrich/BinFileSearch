a454b6a6d7464149dc7d68ab5c9dc5ac14ac60f1
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2023-12-10 00:30:52 +0100

    Java cleanup

--- java/src/PwnedPasswordChecker.java
+++ java/src/PwnedPasswordChecker.java
@@ -7,7 +7,7 @@ import java.security.NoSuchAlgorithmException;
 public class PwnedPasswordChecker {
     private final static String path = "D:/pwned-passwords/pwned-passwords-sha1-ordered-by-hash-full.txt";
 
-    private static long binarySearch(final RandomAccessFile haystack, final String needle, long start, long end) throws IOException {
+    private static long binarySearch(RandomAccessFile haystack, String needle, long start, long end) throws IOException {
         if (start > end) return 0;
 
         final long middle = (start + end) / 2;
@@ -20,18 +20,18 @@ public class PwnedPasswordChecker {
         final String hash = line.substring(0, 40);
 
         return needle.equals(hash) ? Long.parseLong(line.substring(41)) :
-        needle.compareTo(hash) < 0 ? binarySearch(haystack, needle, start, middle - 1) :
-        /* needle.compareTo(hash) > 0 ? */ binarySearch(haystack, needle, middle + 1, end);
+                needle.compareTo(hash) < 0 ? binarySearch(haystack, needle, start, middle - 1) :
+                        /* needle.compareTo(hash) > 0 ? */ binarySearch(haystack, needle, middle + 1, end);
     }
 
-    private static long getCount(final String needle) throws IOException {
+    private static long getCount(String needle) throws IOException {
         try (RandomAccessFile haystack = new RandomAccessFile(path, "r")) {
             final long size = haystack.length();
             return binarySearch(haystack, needle, 0, size);
         }
     }
 
-    private static String getHash(final String needle) throws NoSuchAlgorithmException {
+    private static String getHash(String needle) throws NoSuchAlgorithmException {
         final MessageDigest digest = MessageDigest.getInstance("SHA-1");
         final byte[] hash = digest.digest(needle.getBytes(StandardCharsets.UTF_8));
 
