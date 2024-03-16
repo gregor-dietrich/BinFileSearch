f0bcdbae3768753a96655a8965d162d783beb9b8
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2024-03-16 01:05:48 +0100

    Add C++ Benchmarking

--- cpp/BinFileSearch.cpp
+++ cpp/BinFileSearch.cpp
@@ -1,5 +1,8 @@
 #include <algorithm>
+#include <chrono>
 #include <filesystem>
+#include <functional>
+#include <sstream>
 
 #include "sha1.h"
 
@@ -38,6 +41,34 @@ std::string getHash(const std::string& plaintext) {
 	return hash;
 }
 
+long long benchmark(std::function<void()> func) {
+	auto start = std::chrono::high_resolution_clock::now();
+	func();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start);
+	return duration.count();
+}
+
+void searchPasswords(int argc, char** argv) {
+	std::ifstream fileStream;
+	fileStream.open(argv[1]);
+
+	if (!fileStream.good()) {
+		throw std::runtime_error("Error: Couldn't create input file stream.");
+	}
+
+	std::stringstream ss;
+	for (int i = 2; i < argc; i++) {
+		const auto hash = getHash(argv[i]);
+		ss << "Password: " << argv[i] << '\n' << "Hash: " << getHash(hash) << '\n'
+		   << "Count: " << getCount(fileStream, hash) << "\n\n";
+	}
+	std::cout << ss.str() << '\n';
+
+	if (fileStream.is_open()) {
+		fileStream.close();
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc < 3) {
 		std::cerr << "Error: " << argc << " arguments provided, but at least 3 are required! Arguments found: \n";
@@ -47,38 +78,43 @@ int main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 
-	const auto path = argv[1];
-
-	if (!std::filesystem::exists(path)) {
-		std::cerr << "Error: File does not exist: " << path << '\n';
+	if (!std::filesystem::exists(argv[1])) {
+		std::cerr << "Error: File does not exist: " << argv[1] << '\n';
 		return EXIT_FAILURE;
 	}
 
-	if (!std::filesystem::is_regular_file(path)) {
-		std::cerr << "Error: " << path << " is not a file!\n";
+	if (!std::filesystem::is_regular_file(argv[1])) {
+		std::cerr << "Error: " << argv[1] << " is not a file!\n";
 		return EXIT_FAILURE;
 	}
 
-	std::ifstream haystack;
-	try {
-		haystack.open(path);
-
-		if (!haystack.good()) {
-			throw std::runtime_error("Error: Couldn't create input file stream.");
+	// cold runs
+	long long coldResult = 0;
+	{
+		try {
+			coldResult = benchmark([&]() { searchPasswords(argc, argv); });
 		}
-
-		for (int i = 2; i < argc; i++) {
-			std::cout << "Password: " << argv[i] << '\n' << "Hash: " << getHash(argv[i]) << '\n'
-			<< "Count: " << getCount(haystack, getHash(argv[i])) << "\n\n";
+		catch (const std::runtime_error& err) {
+			std::cerr << err.what() << '\n';
+			return EXIT_FAILURE;
 		}
-	} catch (const std::runtime_error& err) {
-		std::cerr << err.what() << '\n';
-		return EXIT_FAILURE;
 	}
 
-	if (haystack.is_open()) {
-		haystack.close();
+	// warm runs
+	const int runs = 1000;
+	long long warmResult = 0;
+	for (int i = 0; i < runs; i++) {
+		try {
+			warmResult += benchmark([&]() { searchPasswords(argc, argv); });
+		}
+		catch (const std::runtime_error& err) {
+			std::cerr << err.what() << '\n';
+			return EXIT_FAILURE;
+		}
 	}
 
+	std::cout << "Cold run time for 10 passwords: " << coldResult << " microseconds (1 run)\n"
+			  << "Average time taken for 10 passwords: " << warmResult / runs << " microseconds (" + std::to_string(runs) + " runs)\n";
+		
 	return EXIT_SUCCESS;
 }
